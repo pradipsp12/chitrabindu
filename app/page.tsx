08993a042de8d5c1ecf06c61325d348d05ed884e
pradipgithubs <pradipkumarsahoo@userexperiencedesignindia.com>
2025-01-17 13:08:05 +0530

    second

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
         />
         <div className="absolute inset-0 bg-black/40" />
         <Image src={'/logo.jpg'} className="rounded-sm shadow-sm absolute left-12 top-8 shadow-white" width={150} height={40} alt="logo"/>
-        <div className="absolute inset-0 flex flex-col items-start pl-12 justify-center text-white">
+        <div className="absolute md:inset-0 top-40  flex flex-col items-start pl-12 justify-center text-white">
           
-          <h1 className="text-5xl md:text-7xl font-bold my-4">Chitra &nbsp; Bindu<br/> Photography</h1>
+          <h1 className="text-5xl md:text-7xl font-bold my-4 sm:my-1">Chitra &nbsp; Bindu<br/> Photography</h1>
           <p className="text-xl md:text-2xl mb-4">Bengali Wedding Photography</p>
-          <p className="text-lg md:text-xl mb-8">Capturing Timeless Bengali Wedding Traditions</p>
+          <p className="text-sm md:text-xl w-60 md:w-full mb-8">Capturing Timeless Bengali Wedding Traditions</p>
           <Link href={'tel:+917872744407'}  className="bg-red-600 text-white hover:bg-red-700 p-4 rounded-md">
             Call Now: +91 7872744407
           </Link>
